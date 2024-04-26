9ee411886a90d75e50804f0449d95c192486be2f
RAJGOPAL HOTA <110685523+rajgopalhota@users.noreply.github.com>
2024-04-26 21:27:37 +0530

    prod-errs

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const path = require("path"); // Import path module
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 
 // Middleware
 app.use(cors());
@@ -39,6 +39,12 @@ app.use("/api/messages", messagesRoute);
 app.use("/hope", activityRoutes);
 app.use("/pay", payRoutes);
 
+// Route to handle Vercel error
+app.get("/_vercel_error", (req, res) => {
+  // Restart your server or reload necessary components here
+  console.log("Server crashed. Reloading...");
+  process.exit(1); // This will cause Vercel to restart the server
+});
 // Serve the React app for any other routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
